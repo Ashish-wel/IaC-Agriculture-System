7dfe07e9c3d60d647f15ad913d57967f4c0ff5ac
Ashish-wel <ashishnagda34@gmail.com>
2026-02-08 09:52:57 +0530

    Implement basic Flask API for agriculture system

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return "Agriculture System API Running"
+
+@app.route("/soil")
+def soil():
+  return jsonify({
+    "soil" : "Loamy",
+    "moisture" : "Medium"
+  })
+
+@app.route("/crop")
+def crop():
+  return jsonify({
+    "crop" : "Wheat"
+  })
+
+@app.route("/weather")
+def weather():
+  return jsonify({
+    "temperature" : "30c",
+    "humidity" : "60%"
+  })
+
+if __name__ = "__main__":
+  app.run(host="0.0.0.0",port=5000)
+    
